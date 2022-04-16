33a0d5ca7dd0e51904822c8677ee930f7e986ae3
moc1n <1107rizkyeka@gmail.com>
2022-04-16 17:06:25 +0700

    Setting Relasi di forum dan Forum comment model

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+namespace App\Models\User;
+namespace App\Models\ForumComment;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +10,15 @@ use Illuminate\Database\Eloquent\Model;
 class Forum extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+}
+
+public function user()
+{
+  return->$this->belongsTo(User::class);
+}
+public function comments()
+{
+  return->$this->hasMany(ForumsComment::class)
 }
--- app/Models/ForumComment.php
+++ app/Models/ForumComment.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+namespace App\Models\User;
+namespace App\Models\Forum;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +10,12 @@ use Illuminate\Database\Eloquent\Model;
 class ForumComment extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+}
+
+public function user(){
+  return->$this->belongsTo(User::class);
+}
+public function forum(){
+  return->$this->belongsTo(Forum::class);
 }
